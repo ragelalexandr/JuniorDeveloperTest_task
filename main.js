e26947135b6d2a6f40e4648e7b85b22c9eff9872
Alexandr Ragel <alexandr.ragel@outlook.com>
2021-09-05 13:13:27 +0300

    050920211313

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
 //var hw = "hello world";
 //alert(hw);
 
-var hw = 'Hello,World!!!'
 
-var myHeading = document.querySelector('p');
-myHeading.textContent = hw;
+var hw = 'Hello,World!!!'
+var dataArray = ['a = 7.1 - 4 + 2 - 3,b = a + 1,c = a - b',
+          'd = - c + 1 + 5 + a ,,x = a + b + c + d'];
+var stringArray = [];
+var varArray = [];
+
+for(var i = 0; i < dataArray.length; i++) {
+  stringArray = dataArray[i].split(',')
+  for (var j = 0; j < stringArray.length; j++) {
+    varArray.push(stringArray[j]);
+  }
+}
+
+var $table = document.createElement("table"), $row;
+
+for(var i = 0; i < varArray.length; i++) {
+  $row = $table.insertRow(-1);
+  $col = $row.insertCell(-1);
+  $col.innerText = varArray[i];
+}
+
+document.body.appendChild($table);
+
+console.log(stringArray);
+console.log(varArray);
